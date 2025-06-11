874e6ac42e8459fd478c058a2ff596468e5bd548
Anuragg Kamdi <138967822+AnuraggKamdi@users.noreply.github.com>
2025-06-11 17:17:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket-001"
-  project                  = "tt-dev-001"
+  name                     = "tf-githubdemo-bucket-001"
+  project                  = "My-GCP-Project"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
 #
 resource "google_storage_bucket" "my-bucket2" {
-  name                     = "tt-githubdemo-bucket-002"
-  project                  = "tt-dev-001"
+  name                     = "tf-githubdemo-bucket-002"
+  project                  = "My-GCP-Project"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
-#
-### dummy
+

ccefba0842e65c4ba7a1d907f131c9d85e122e58
Anuragg Kamdi <138967822+AnuraggKamdi@users.noreply.github.com>
2025-06-11 17:44:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,10 @@ resource "google_storage_bucket" "my-bucket2" {
   public_access_prevention = "enforced"
 }
 
+resource "google_storage_bucket" "my-bucket3" {
+  name                     = "tf-githubdemo-bucket-003"
+  project                  = "My-GCP-Project"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
